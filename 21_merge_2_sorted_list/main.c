8bc616a6ce3977dc9a8170c9bc94b66e736bb5ae
Anhbt <zzanhtuanbuizz@gmail.com>
2025-09-29 12:26:17 +0700

    21_merged 2 sorted list -- 100%

--- 21_merge_2_sorted_list/main.c
+++ 21_merge_2_sorted_list/main.c
@@ -1,58 +1,128 @@
 #include <stdint.h>
 #include <stdio.h>
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
+#include <stdlib.h>
 
 struct ListNode {
     int val;
     struct ListNode* next;
 };
+struct ListNode* init_node(int val, struct ListNode* next);
+void print_ll(struct ListNode* head);
+void free_ll(struct ListNode* head);
+struct ListNode* init_ll_no_cycle(int* arr, int arr_size);
 
-struct ListNode* expand_the_list(struct ListNode* const list, const int* const new_val) {
-    struct ListNode* tmp = (struct ListNode*)malloc(sizeof(struct ListNode));
-    tmp->val = new_val;
-    tmp->next = NULL;
-    list->next = tmp;
+/**
+ * Implement
+ */
+struct ListNode* init_node(int val, struct ListNode* next) {
+    struct ListNode* ret = (struct ListNode*)malloc(sizeof(struct ListNode));
+    ret->val = val;
+    ret->next = next;
+    return ret;
+}
+
+void print_ll(struct ListNode* head) {
+    struct ListNode* traverser = head;
+    printf("The linked list [val; next]: ");
+    while (traverser) {
+        printf("[%d; %p] -> ", traverser->val, traverser->next);
+        traverser = traverser->next;
+    }
+    printf("NULL\n");
+}
+
+void free_ll(struct ListNode* head) {
+    struct ListNode* traverse = head;
+    struct ListNode* next;
+    while (traverse != NULL) {
+        next = traverse->next;
+        free(traverse);
+        traverse = next;
+    }
+}
+
+struct ListNode* init_ll_no_cycle(int* arr, int arr_size) {
+    struct ListNode* head = init_node(arr[0], NULL);
+    printf("Init cycle linked list: %d -> ", head->val);
+    if (arr_size < 2) goto exit;
+    struct ListNode* cur = head;
+    struct ListNode* tmp;
+
+    int i;
+    for (i = 1; i < arr_size; i++) {
+        tmp = init_node(arr[i], NULL);
+        cur->next = tmp;  // update tail of ll
+        cur = tmp;        // curr now at the tail
+        printf("%d -> ", tmp->val);
+    }
+
+exit:
+    printf("NULL\n");
+    return head;
 }
 
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
     struct ListNode* trace1 = list1;
     struct ListNode* trace2 = list2;
-    int next_val = 0;
-    struct ListNode* ret;
+    struct ListNode* ret = NULL;
+    struct ListNode* traverser;
 
-    // do the algorithm
-    while (trace1 == NULL || trace2 == NULL) {
+    if (list1 != NULL & list2 != NULL) {
         if (trace1->val < trace2->val) {
-            next_val = trace1->val;
+            ret = list1;
+            traverser = list1;
             trace1 = trace1->next;
         } else {
-            next_val = trace2->val;
+            ret = list2;
+            traverser = list2;
             trace2 = trace2->next;
         }
-
-        expand_the_list(ret, &next_val);
+    } else if (list1 != NULL && list2 == NULL) {
+        ret = list1;
+        goto exit;
+    } else if (list1 == NULL && list2 != NULL) {
+        ret = list2;
+        goto exit;
+    } else {
+        goto exit;
     }
 
-    if (trace1 != NULL) {
-        while (trace1 == NULL) {
-            expand_the_list(ret, &next_val);
+    // do the algorithm
+    while (trace1 && trace2) {
+        if (trace1->val < trace2->val) {
+            traverser->next = trace1;
             trace1 = trace1->next;
+        } else {
+            traverser->next = trace2;
+            trace2 = trace2->next;
         }
+        traverser = traverser->next;
     }
 
-    if (trace2 != NULL) {
-        while (trace2 == NULL) {
-            expand_the_list(ret, &next_val);
-            trace1 = trace2->next;
-        }
+    if (trace1 == NULL) {
+        traverser->next = trace2;
     }
 
+    if (trace2 == NULL) {
+        traverser->next = trace1;
+    }
+
+exit:
     return ret;
+}
+
+int main() {
+    int arr1[] = {1, 2, 4};
+    int arr1_size = sizeof(arr1) / sizeof(arr1[0]);
+    int arr2[] = {1, 3, 4};
+    int arr2_size = sizeof(arr1) / sizeof(arr2[0]);
+
+    struct ListNode* list1 = init_ll_no_cycle(arr1, arr1_size);
+    struct ListNode* list2 = init_ll_no_cycle(arr2, arr2_size);
+
+    struct ListNode* merged = mergeTwoLists(list1, list2);
+
+    print_ll(merged);
+    free_ll(merged);
+    return 0;
 }
